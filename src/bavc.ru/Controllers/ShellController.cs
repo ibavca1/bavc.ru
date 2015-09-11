9f6eebf00d54e0be96bd394b2ae6c91866b10c76
Anton <a.belonogov@retailinform.ru>
2015-09-11 17:03:24 +0500

    Add class Web/WebSite.cs and etc.

--- src/bavc.ru/Controllers/ShellController.cs
+++ src/bavc.ru/Controllers/ShellController.cs
@@ -16,7 +16,6 @@ namespace bavc.ru.Controllers
         {
             ViewBag.Titile = "Shell";
             string o = ShellHelper.RunShell();
-            //string o = "asasas";
             return View((object)o);
         }
 
